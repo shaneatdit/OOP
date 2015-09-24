a311322ec45c8627227d632500f255db6ea85e9b
shaneatdit <shanecroberts@yahoo.ie>
2015-09-24 21:26:25 +0100

    Create triangle.py
    Calculates the length of the hypotenuse of a right-angled triangle

---
+++ triangle.py
@@ -0,0 +1,6 @@
+a = float(input("Length of adjacent side: "))
+o = float(input("Width of opposite side: "))
+h = (a ** 2 + o ** 2) ** 0.5
+
+print("")
+print("The hypotenuse of right-angle triangle with sides ", a, "and", o, "is", h)
