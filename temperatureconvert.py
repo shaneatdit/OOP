af85f6476ee8582c05d3e6f2ca645cfd18522f95
shaneatdit <shanecroberts@yahoo.ie>
2015-09-24 21:28:42 +0100

    Update temperatureconvert.py
    Converts from Celsius to Freedom Units

--- temperatureconvert.py
+++ temperatureconvert.py
@@ -1,4 +1,3 @@
-c = input("Temperature in Celsius: ")
-celsius = int(c)
+celsius = float(input("Temperature in Celsius: "))
 fahr = celsius * 1.8 + 32
 print(celsius, " degrees Celsius is ", fahr, " degrees Fahrenheit")
