245998793e593f62a0136b8ae48f3cb438f303b9
shaneatdit <shanecroberts@yahoo.ie>
2015-09-24 21:08:32 +0100

    Create perimeter.py

---
+++ perimeter.py
@@ -0,0 +1,7 @@
+# madlib.py
+#
+adjective = input("Enter an adjective: ")
+noun = input("Enter an noun: ")
+verb = input("Enter an verb: ")
+adverb = input("Enter an adverb: ")
+print("A", adjective, noun,"should never", verb, adverb)
