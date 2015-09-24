0a00ebe2b4044fb8dcd9c0b898756f6281b0c998
shaneatdit <shanecroberts@yahoo.ie>
2015-09-24 21:23:55 +0100

    Create average.py

---
+++ average.py
@@ -0,0 +1,7 @@
+n1 = float(input("What's the first number? "))
+n2 = float(input("What's the second number? "))
+n3 = float(input("What's the third number? "))
+average = (n1 + n2 + n3) / 3
+
+print("")
+print("The average is", float(average))
