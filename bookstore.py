386c94e8f3efb3d5273727faacb2af843333e8e1
shaneatdit <shanecroberts@yahoo.ie>
2015-09-24 21:29:30 +0100

    Update bookstore.py

--- bookstore.py
+++ bookstore.py
@@ -1,9 +1,13 @@
-p = input("Enter cover price: ")
-coverprice = int(p)
-d = input("Enter discount: ")
-discount = 1 - int(d) / 100
-q = input("Enter quantity: ")
-quantity = int(q)
+coverprice = float(input("Enter cover price: "))
+d = float(input("Enter discount rate: "))
+discount = 1 - d / 100
+quantity = int(input("Enter quantity: "))
 shipping = 2.25 + 0.75 * quantity
 cost = (quantity * coverprice * discount) + shipping
-print("The wholesale cost of ", quantity, "books is", cost)
+print("")
+print(quantity, "books at ", coverprice, "per book:   ", coverprice * quantity)
+print("Discount (", d, "%):            ", quantity * coverprice * d / 100)
+print("Discounted price:              ", quantity * coverprice * discount)
+print("Shipping cost:                 ", shipping)
+print("")
+print("The net cost of", quantity, "books is:   ", "%.2f" % cost)
