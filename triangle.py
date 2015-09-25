bc316cc2cac20493110f8218d82b1fcaa2828c29
shaneatdit <shanecroberts@yahoo.ie>
2015-09-25 13:52:21 +0100

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -4,13 +4,19 @@ __author__ = 'D15124888'
 a = 0
 while a <= 0:
     a = float(input("Length of adjacent side: "))
+    if a <= 0:
+        print("")
+        print("Please enter a positive number")
 o = 0
 while o <= 0:
     o = float(input("Width of opposite side: "))
+    if o <= 0:
+        print("")
+        print("Please enter a positive number")
 
 # calculate hypotenuse length
 h = (a ** 2 + o ** 2) ** 0.5
 
 # pint answer
 print("")
-print("The hypotenuse of a right-angle triangle with sides ", a, "and", o, "is", h)
+print("The hypotenuse of right-angle triangle with sides ", a, "and", o, "is", h)
