ccce63217d13a9bc09742e2075386dd010bcf87d
shaneatdit <shanecroberts@yahoo.ie>
2015-10-14 17:23:42 +0100

    Delete madlib.py

--- madlib.py
+++
@@ -1,7 +0,0 @@
-# madlib.py
-#
-adjective = input("Enter an adjective: ")
-noun = input("Enter an noun: ")
-verb = input("Enter an verb: ")
-adverb = input("Enter an adverb: ")
-print("A", adjective, noun,"should never", verb, adverb)
