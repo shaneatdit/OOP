76973c04bb4abf1c6f050b94a27c42d3704a8d3b
shaneatdit <shanecroberts@yahoo.ie>
2015-10-30 18:08:55 +0000

    Fixed typo

--- wotd.py
+++ wotd.py
@@ -9,14 +9,14 @@
 from random import randint
 
 
-# Creates a list of words from file
 def wordlist(file):
+    # Creates a list of words from file
     with open(file) as f:
         return f.read().split()
 
 
-# Delects a word at random from dictionary
 def main():
+    # Selects a word at random from dictionary
     word_list = wordlist("dictionary.txt")
     todays_word = randint(0, len(word_list) - 1)
     print("Today's word is:", word_list[todays_word])
